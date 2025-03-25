2fa93df7623864421a19f3da7e7e1859c437a029
Ashutoshhhhh <ashutoshhnegi@gmail.com>
2025-03-25 12:50:49 +0530

    added mongo uri

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors=require('cors');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+if(!process.env.MONGO_URI){
+    console.error("Mongo url is not defined in Env file");
+    process.exit(1);
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app= express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+
+
+async function startServer(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Mongo Connected');
+        const port = process.env.PORT || 5000;
+        app.listen(port,()=>console.log(`App is running on http://localhost:${port}`))
+    }
+    catch(err){
+        console.log(`Error in connection to backend check mongo maybe ${err}`);
+    }
+}
+startServer();
